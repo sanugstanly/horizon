e67052ec76ba4479e080e6fa675f75096d430b6d
sanustanly <sanu@fresh.sa>
2021-05-22 20:24:07 +0300

    change for adding token request in horizon

--- resources/js/app.js
+++ resources/js/app.js
@@ -22,10 +22,10 @@ if (token) {
     axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
 }
 
-axios.defaults.headers.common['Authorization'] = `Bearer ${window.$cookies.get('auth_token')}`
-
 Vue.use(VueRouter);
 Vue.use(VueCookies)
+console.log(window.$cookies.get('auth_token'))
+axios.defaults.headers.common['Authorization'] = `Bearer ${window.$cookies.get('auth_token')}`
 
 Vue.prototype.$http = axios.create();
 
